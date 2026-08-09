e0ba26eb42cd663d8c4207fecb0bca071320574d
Raul Hidalgo <deinok@deinok.com>
2026-08-09 18:57:28 +0200

    .

--- src/ScarletRadioControl.Web.Frontend/src/hooks/useRtcPeerConnection.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/hooks/useRtcPeerConnection.tsx
@@ -14,10 +14,10 @@ export default function useRtcPeerConnection(): {rtcPeerConnection: RTCPeerConne
 		return { 
 			rtcPeerConnection, 
 			rtcDataChannels: { 
-				commandsRtcDataChannel: rtcPeerConnection.createDataChannel("commands", { negotiated: true, id: 1 }), 
-				controlRtcDataChannel: rtcPeerConnection.createDataChannel("control", { ordered: false, maxRetransmits: 0, negotiated: true, id: 0 }), 
-				eventsRtcDataChannel: rtcPeerConnection.createDataChannel("events", { negotiated: true, id: 3 }),
-				telemetryRtcDataChannel: rtcPeerConnection.createDataChannel("telemetry", { ordered: false, maxRetransmits: 0, negotiated: true, id: 2 }), 
+				commandsRtcDataChannel: rtcPeerConnection.createDataChannel("commands", { id: 1, negotiated: true, }), 
+				controlRtcDataChannel: rtcPeerConnection.createDataChannel("control", { id: 0,  maxRetransmits: 0, negotiated: true, ordered: false, }), 
+				eventsRtcDataChannel: rtcPeerConnection.createDataChannel("events", { id: 3, negotiated: true, }),
+				telemetryRtcDataChannel: rtcPeerConnection.createDataChannel("telemetry", { id: 2, maxRetransmits: 0, negotiated: true, ordered: false, }), 
 			} 
 		};
 	});
