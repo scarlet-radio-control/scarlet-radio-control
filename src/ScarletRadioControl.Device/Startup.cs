7a6bb293f2214c8ea9d99d503d6ff62bd20f6366
Raul Hidalgo Caballero <deinok@deinok.com>
2026-08-10 20:06:03 +0200

    ScarletRadioControl.Device (#87)
    * .
    
    * .
    
    * .
    
    * .
    
    * .

---
+++ src/ScarletRadioControl.Device/Startup.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using ScarletRadioControl.Device.BackgroundServices;
+
+namespace ScarletRadioControl.Device;
+
+public static class Startup
+{
+
+	public static void ConfigureServices(IServiceCollection serviceCollection)
+	{
+		serviceCollection.AddHostedService<WorkerBackgroundService>();
+	}
+
+}
